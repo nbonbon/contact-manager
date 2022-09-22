fcbb01a93d901aeae2c5173021ab94f9ec73b8da
Nick Bonnet <njbuwm@gmail.com>
2022-09-21 20:30:14 -0400

    ContactListManager unit tests for add and delete

--- test/contact_manager/ContactListManagerTest.cpp
+++ test/contact_manager/ContactListManagerTest.cpp
@@ -1,9 +1,31 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include "ContactListManager.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(ContactListManagerTest, AddContact_shouldAddAContactToTheList) 
+{
+  ContactListManager listManager;
+
+  Contact contact(std::string("john doe"), std::string("888-888-8888"));
+  listManager.AddContact(contact);
+  
+  std::vector<Contact> contactList = listManager.GetList();
+  EXPECT_EQ(contactList.size(), 1);
+  EXPECT_EQ(contactList.at(0).GetName(), "john doe");
+  EXPECT_EQ(contactList.at(0).GetPhoneNumber(), "888-888-8888");
+}
+
+TEST(ContactListManagerTest, DeleteContactByName_shouldDeleteAContactFromTheList) 
+{
+  ContactListManager listManager(std::vector<Contact>( {
+    Contact(std::string("john doe"), std::string("888-888-8888")),
+    Contact(std::string("jane doe"), std::string("888-888-8881"))
+  }));
+
+  listManager.DeleteContactByName("john doe");
+  
+  std::vector<Contact> contactList = listManager.GetList();
+  EXPECT_EQ(contactList.size(), 1);
+  EXPECT_EQ(contactList.at(0).GetName(), "jane doe");
+  EXPECT_EQ(contactList.at(0).GetPhoneNumber(), "888-888-8881");
 }
